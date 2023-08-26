009e5dd4e23201545deb534cc670b9a54c228379
rafaelpernil2 <mysterysynthman@gmail.com>
2023-08-26 23:33:27 +0200

    fix: change openfaas template

--- buildMultiArch.sh
+++ buildMultiArch.sh
@@ -2,5 +2,5 @@
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 export DOCKER_CLI_EXPERIMENTAL=enabled
 rm -rf ./template
-faas-cli template pull https://github.com/rafaelpernil2/openfaas-template-node-typescript-express
+faas-cli template pull https://github.com/rafaelpernil2/openfaas-template-node-typescript-uwebsockets
 faas-cli publish -f https-graphql-users.yml --platforms linux/arm/v7
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf ./template
-faas-cli template pull https://github.com/rafaelpernil2/openfaas-template-node-typescript-express
+faas-cli template pull https://github.com/rafaelpernil2/openfaas-template-node-typescript-uwebsockets
 faas-cli deploy -f ./https-graphql-users.yml
